11a782b5da1adbb10a0e7d64b26429ae972a5b67
KDW <42798237+dawan0111@users.noreply.github.com>
2022-10-28 16:47:21 +0900

    wave animation 추가

--- src/components/WordItem/WordItem.tsx
+++ src/components/WordItem/WordItem.tsx
@@ -1,10 +1,13 @@
+import Wave from 'react-wavify';
+
 import { Button } from 'antd';
 import clsx from 'clsx';
+import { useTheme } from 'styled-components';
 
 import useAudio from '~/hooks/useAudio';
 import { Word } from '~/types/word';
 
-import { KoreanText, WordItemStyled } from './styled';
+import { BackgroundWave, KoreanText, WordItemStyled } from './styled';
 
 export interface WordItemProps extends Word {
   className?: string;
@@ -12,9 +15,10 @@ export interface WordItemProps extends Word {
 }
 
 const WordItem = ({ className, english, korean, day, audioLink, hiddenKorean }: WordItemProps) => {
-  const { toggle } = useAudio(audioLink);
+  const { playing, toggle } = useAudio(audioLink);
+  const theme = useTheme();
   return (
-    <WordItemStyled className={clsx('WordItem', className)}>
+    <WordItemStyled className={clsx('WordItem', className)} playing={playing}>
       <div className="volume">
         <Button
           onClick={() => {
@@ -28,6 +32,29 @@ const WordItem = ({ className, english, korean, day, audioLink, hiddenKorean }: 
       <div className="korean">
         <KoreanText hiddenText={Boolean(hiddenKorean)}>{korean}</KoreanText>
       </div>
+      <BackgroundWave playing={playing}>
+        <Wave
+          mask="url(#mask)"
+          fill={theme.colors.primary}
+          paused={!playing}
+          options={{
+            height: 30,
+            amplitude: 10,
+            speed: 0.5,
+            points: 3,
+          }}
+        >
+          <defs>
+            <linearGradient id="gradient" gradientTransform="rotate(90)">
+              <stop offset="0" stopColor="white" />
+              <stop offset="0.5" stopColor="black" />
+            </linearGradient>
+            <mask id="mask">
+              <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)" />
+            </mask>
+          </defs>
+        </Wave>
+      </BackgroundWave>
     </WordItemStyled>
   );
 };
